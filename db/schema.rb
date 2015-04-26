b5b44d38755239bae2e37bba8e2594c072fe91ca
Zuri Hunter <zurihunter92@gmail.com>
2015-04-26 02:46:20 +0000

    Fixed validations and connections for listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425215947) do
+ActiveRecord::Schema.define(version: 20150426022305) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150425215947) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
